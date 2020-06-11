c4cc610444e34e38934c9dd2d1ab5fd0b1254ef3
Carlos Leite <CarlosLeite@criticalmanufacturing.com>
2020-06-11 22:39:15 +0100

    End of section 3

---
+++ Infrastructure/Data/Config/ProductConfiguration.cs
@@ -0,0 +1,24 @@
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Data.Config
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.Property(p => p.Id).IsRequired();
+            builder.Property(p => p.Name).IsRequired().HasMaxLength(100);
+            builder.Property(p => p.Description).IsRequired().HasMaxLength(100);
+            builder.Property(p => p.Price).HasColumnType("decimal(18,2)");
+            builder.Property(p => p.PictureUrl).IsRequired();
+            
+            // This is just an example, entity framework already manages this connections
+            builder.HasOne(p => p.Productbrand).WithMany()
+                .HasForeignKey(p => p.ProductBrandId);
+            builder.HasOne(p => p.ProductType).WithMany()
+                .HasForeignKey(p => p.ProductTypeId);
+        }
+    }
+}
