7447c3f426db2ae27a2b4aa6154ba84840925da0
Carlos Leite <CarlosLeite@criticalmanufacturing.com>
2020-06-11 22:36:40 +0100

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
@@ -28,6 +29,7 @@ namespace API
         // Dependencies injector containers
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<IProductRepository, ProductRepository>();
             services.AddControllers();
             services.AddDbContext<StoreContext>(x => x.UseSqlite(
                 _config.GetConnectionString("DefaultConnection")
