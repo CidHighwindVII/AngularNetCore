0e84d7b1de6da2a38f389b82e282d2a175b37cfd
Carlos Leite <CarlosLeite@criticalmanufacturing.com>
2020-07-29 00:14:24 +0100

    End of section 9

---
+++ client/src/app/shop/shop.component.ts
@@ -0,0 +1,107 @@
+import { IBrand } from '../shared/Models/brand';
+import { IType } from '../shared/Models/productType';
+import { ShopService } from './shop.service';
+import { IProduct } from './../shared/Models/product';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { ShopParams } from '../shared/Models/shopParams';
+import { ThrowStmt } from '@angular/compiler';
+
+@Component({
+  selector: 'app-shop',
+  templateUrl: './shop.component.html',
+  styleUrls: ['./shop.component.scss'],
+})
+export class ShopComponent implements OnInit {
+  @ViewChild('search', { static: true }) searchTerm: ElementRef;
+  products: IProduct[];
+  types: IType[];
+  brands: IBrand[];
+  shopParams = new ShopParams();
+  totalCount: number;
+
+  sortOptions = [
+    { name: 'Alphabetical', value: 'name' },
+    { name: 'Price: Low to High', value: 'priceAsc' },
+    { name: 'Price: High to Low', value: 'priceDesc' },
+  ];
+
+  constructor(private shopService: ShopService) {}
+
+  ngOnInit(): void {
+    this.getProducts();
+    this.getBrands();
+    this.getTypes();
+  }
+
+  getProducts(): void {
+    this.shopService.getProducts(this.shopParams).subscribe(
+      (response) => {
+        this.products = response.data;
+        this.shopParams.pageNumber = response.pageIndex;
+        this.shopParams.pageSize = response.pageSize;
+        this.totalCount = response.count;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  getBrands(): void {
+    this.shopService.getTypes().subscribe(
+      (response) => {
+        this.brands = [{ id: 0, name: 'All' }, ...response];
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  getTypes(): void {
+    this.shopService.getBrands().subscribe(
+      (response) => {
+        this.types = [{ id: 0, name: 'All' }, ...response];
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  onBrandSelected(brandId: number): void {
+    this.shopParams.brandId = brandId;
+    this.shopParams.pageNumber = 1;
+    this.getProducts();
+  }
+
+  onTypeSelected(typeId: number): void {
+    this.shopParams.typeId = typeId;
+    this.shopParams.pageNumber = 1;
+    this.getProducts();
+  }
+
+  onSortSelected(sort: string): void {
+    this.shopParams.sort = sort;
+    this.getProducts();
+  }
+
+  onPageChanged(event: any): void {
+    if (this.shopParams.pageNumber != event) {
+      this.shopParams.pageNumber = event;
+      this.getProducts();
+    }
+  }
+
+  onSearch() {
+    this.shopParams.search = this.searchTerm.nativeElement.value;
+    this.shopParams.pageNumber = 1;
+    this.getProducts();
+  }
+
+  onReset() {
+    this.searchTerm.nativeElement.value = '';
+    this.shopParams = new ShopParams();
+    this.getProducts();
+  }
+}
